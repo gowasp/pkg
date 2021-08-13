cd5732443b3d64845422edab8546aeb4f55b0733
tnngo <tnngou@gmail.com>
2021-08-13 20:00:57 +0800

    update: Type change Fixed

---
+++ pact.go
@@ -0,0 +1,86 @@
+package pkg
+
+import (
+	"encoding/binary"
+	"errors"
+)
+
+type Fixed byte
+
+const (
+	FIXED_CONNECT Fixed = iota + 1
+	FIXED_CONNACK
+	FIXED_PING
+	FIXED_PONG
+	FIXED_PUBLISH
+	FIXED_PUBACK
+	FIXED_SUBSCRIBE
+	FIXED_SUBACK
+	FIXED_UNSUBSCRIBE
+	FIXED_UNSUBACK
+	FIXED_PVTPUBLISH
+	FIXED_PVTPUBACK
+	FIXED_FORWARD
+)
+
+var (
+	ErrVarintOutOfRange = errors.New("varint out of range")
+)
+
+func EncodeVarint(x int) []byte {
+	var buf [5]byte
+	var n int
+	for n = 0; x > 127; n++ {
+		buf[n] = 0x80 | uint8(x&0x7F)
+		x >>= 7
+	}
+
+	if n > 4 {
+		return nil
+	}
+	buf[n] = uint8(x)
+	n++
+	return buf[0:n]
+}
+
+func DecodeVarint(b []byte) (int, int) {
+	u, i := binary.Uvarint(b)
+	return int(u), i
+}
+
+func (f Fixed) Encode(body []byte) []byte {
+	ebody := append(EncodeVarint(len(body)), body...)
+	cbody := append([]byte{byte(f)}, ebody...)
+	return cbody
+}
+
+func PubDecode(body []byte) (int, string, []byte) {
+	v, n := DecodeVarint(body)
+	return v, string(body[n+1 : n+1+int(body[n])]), body[n+1+int(body[n]):]
+}
+
+func PubEncode(seq int, topic string, body []byte) []byte {
+	t := append([]byte(topic), body...)
+	tl := len([]byte(topic))
+	t1 := append([]byte{byte(tl)}, t...)
+	s := append(EncodeVarint(seq), t1...)
+
+	b := append(EncodeVarint(len(s)), s...)
+	return append([]byte{byte(FIXED_PUBLISH)}, b...)
+}
+
+// int: seq.
+// byte: topicID.
+// []byte: remaining content.
+func PvtPubDecode(body []byte) (int, byte, []byte) {
+	v, n := DecodeVarint(body)
+	return v, body[n], body[n+1:]
+}
+
+func PvtPubAckEncode(seq int) []byte {
+	seqBody := EncodeVarint(seq)
+
+	v := append(EncodeVarint(len(seqBody)), seqBody...)
+
+	return append([]byte{byte(FIXED_PVTPUBACK)}, v...)
+}
---
+++ subscribe.go
@@ -0,0 +1,47 @@
+package pkg
+
+import (
+	"context"
+	"sync"
+)
+
+type SubFunc func(context.Context, []byte) error
+
+type Subscribe struct {
+	subMap sync.Map
+}
+
+func (s *Subscribe) Subscribe(topic string, f SubFunc) {
+	s.subMap.Store(topic, f)
+}
+
+func (s *Subscribe) Get(topic string) SubFunc {
+	if v, ok := s.subMap.Load(topic); ok {
+		return v.(SubFunc)
+	}
+	return nil
+}
+
+func (s *Subscribe) GetTopics() []string {
+	strs := make([]string, 0)
+	s.subMap.Range(func(key, value interface{}) bool {
+		strs = append(strs, key.(string))
+		return true
+	})
+	return strs
+}
+
+type Private struct {
+	subMap sync.Map
+}
+
+func (ps *Private) Subscribe(topicID byte, f SubFunc) {
+	ps.subMap.Store(topicID, f)
+}
+
+func (ps *Private) Get(topicID byte) SubFunc {
+	if v, ok := ps.subMap.Load(topicID); ok {
+		return v.(SubFunc)
+	}
+	return nil
+}
